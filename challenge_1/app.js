aeafb1c2747d96ecafdba6d48b18d2159d67f905
Jessica <jessicarhodson@gmail.com>
2018-12-10 12:18:03 -0800

    Add turn handling to switch players

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,35 @@
+// On Load
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var playerElement = document.getElementsByClassName('player')[0];
+    playerElement.innerHTML += "Player X: Your Move";
+
+    turnHandler();
+})
+
+/* ------------ Game Play --------------------- */
+
+/* Turns */
+
+var turnHandler = function() {
+  var boxes = document.getElementsByClassName('box')
+
+  for(var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', changePlayer);
+  }
+}
 
 
+/* ------------ Reset --------------------- */
+
+
+/* ------------ Helper Functions --------------------- */
+
+var changePlayer = function() {
+  var playerElement = document.getElementsByClassName('player')[0]
+  if (playerElement.innerHTML === "Player X: Your Move") {
+    playerElement.innerHTML = "Player O: Your Move";
+  } else if (playerElement.innerHTML === "Player O: Your Move") {
+    playerElement.innerHTML = "Player X: Your Move";
+  }
+}

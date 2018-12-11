9db7817cca96821f896242f50eedbb6a1db3cc21
Jessica <jessicarhodson@gmail.com>
2018-12-11 12:24:18 -0800

    Add to .gitignore, build server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,16 @@
+$(document).ready(function() {
+  console.log('ready!')
 
+})
+
+// listen for click on submit button
+//  on submit, send post request
+
+// display incoming CSV data from server
+//  append text to the CSV_display area
+
+
+// post request
+//  send data in JSON format to server
+//  on completion, call display handler
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+app.use(express.static('client'))
 
 
+// respond to get request
+app.get('/', (req, res)=> {
+  // res.send('client/index.html');
+})
+//  send index.html
+
+
+// respond to post request
+//  receive incoming data
+//  parse?
+//  convert to CSV 
+//  send back CSV data
+//  send back empty form?
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+})
+// build server

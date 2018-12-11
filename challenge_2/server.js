8d9904f58eee89d424b97a8003d188f9013af427
Jessica <jessicarhodson@gmail.com>
2018-12-11 12:42:57 -0800

    Outline json-csv converter function, get keys of json

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,19 +5,40 @@ app.use(express.static('client'))
 
 
 // respond to get request
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   // res.send('client/index.html');
 })
-//  send index.html
-
 
 // respond to post request
+app.post('/', (req, res) => {
 //  receive incoming data
-//  parse?
+  jsonToCSVConverter(req.body, (result) => {
+    res.send(result)
+  })
 //  convert to CSV 
 //  send back CSV data
+  
+})
+//  parse?
 //  send back empty form?
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 })
 // build server
+
+
+var jsonToCSVConverter = function(json, callback) {
+  var CSV = '';
+  // generate string of keys separated by commas
+  var keys = Object.keys(json);
+
+  for (var i = 0; i < keys.length-2; i++) {
+    CSV += keys[i] + ',';
+  }
+  CSV += keys[keys.length-2];
+  callback(CSV);
+}
+
+var getKeys = function(json) {
+
+}

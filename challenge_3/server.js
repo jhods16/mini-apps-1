aa6ff984bd68f6aed94e4751b29853d98b6b06d9
Jessica <jessicarhodson@gmail.com>
2018-12-12 11:20:39 -0800

    Init package.json, index.html, server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('recevied')
+})
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
 
 

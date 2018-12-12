a40bc884f664846feeb13f89404fa5c1b2d310f8
Jessica <jessicarhodson@gmail.com>
2018-12-12 14:39:17 -0800

    Set h1, set up initial state

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,65 @@
-console.log('stuff')
 
-var name = 'Jess';
-var ele = <h1>Hello {name}</h1>;
+class App extends React.Component {
 
-ReactDOM.render(ele, document.getElementById('test'))
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			login: {
+				name: '',
+				email: '',
+				password: ''
+			},
+			address: {
+				line1: '',
+				line2: '',
+				city: '',
+				state: '',
+				zip: '',
+				phone: ''
+			},
+			payment: {
+				cc: '',
+				expdate: '',
+				cvv: '',
+				billzip: ''
+			}
+		};
+		this.handleCheckout = this.handleCheckout.bind(this);
+		this.handleNext = this.handleNext.bind(this)
+	}
+
+	handleCheckout(event) {
+		alert('you clicked checkout');
+		this.setState({login:{name: 'Jess'}})
+	}
+
+	handleNext(event) {
+
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>{this.state.login.name}</h2>
+				<button onClick={this.handleCheckout}>Checkout</button>
+			</div>
+		)
+	}
+	
+}
+
+
+var login = function(props) {
+	<div>Log In
+		<form>
+			<input type="text" name="name"/>
+			<input type="text" name="email"/>
+			<input type="text" name="password"/>
+		</form>
+	</div>
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
+

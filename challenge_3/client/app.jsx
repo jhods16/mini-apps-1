5b0ecd35ac18c2faa9dd626526c0e198a48d7b17
Jessica <jessicarhodson@gmail.com>
2018-12-12 13:04:38 -0800

    Add react and render app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,6 @@
+console.log('stuff')
 
+var name = 'Jess';
+var ele = <h1>Hello {name}</h1>;
 
+ReactDOM.render(ele, document.getElementById('test'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('recevied')

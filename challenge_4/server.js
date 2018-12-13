88a30c66469dc19f80db78439b058747ff21675b
Jessica <jessicarhodson@gmail.com>
2018-12-13 12:38:31 -0800

    Add webpack-associated files, config

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('client/dist'));
 
 app.listen(port, (err) => {
   if (err) return console.error(err);

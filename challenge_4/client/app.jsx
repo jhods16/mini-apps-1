2f8ca083585a740b4b67ce111b700a47dcce8912
Jessica <jessicarhodson@gmail.com>
2018-12-13 12:10:41 -0800

    Make master plan and set up express, react, babel, index.html

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,16 @@
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <h1>Rendered</h1>
+      )
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
+var port = 3000;
 
+app.use(express.static('public'));
 
+app.listen(port, (err) => {
+  if (err) return console.error(err);
+  console.log(`listening on ${port}!`);
+})

afa97e84ad4830979e2c513b4e50ad35865f7761
Jessica <jessicarhodson@gmail.com>
2018-12-12 16:02:02 -0800

    Add master plan doc, enable movement between pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -4,14 +4,14 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			name: '',
+			formnum: 0,
 			login: {
-				name: '',
-				email: '',
-				password: ''
+				name: 'Jess',
+				email: 'email.com',
+				password: '123'
 			},
 			address: {
-				line1: '',
+				line1: '123 Maple St.',
 				line2: '',
 				city: '',
 				state: '',
@@ -26,38 +26,107 @@ class App extends React.Component {
 			}
 		};
 		this.handleCheckout = this.handleCheckout.bind(this);
-		this.handleNext = this.handleNext.bind(this)
+		this.handleNext = this.handleNext.bind(this);
+		this.returnHome = this.returnHome.bind(this);
 	}
 
 	handleCheckout(event) {
-		alert('you clicked checkout');
-		this.setState({login:{name: 'Jess'}})
+		this.setState({formnum: 1})
 	}
 
 	handleNext(event) {
+		var formnum = this.state.formnum
+		this.setState({formnum: formnum+1})
+	}
 
+	returnHome(event) {
+		this.setState({formnum: 0})
 	}
 
 	render() {
+		var page;
+		var formnum = this.state.formnum;
+
+		if (formnum === 0) {
+			page = <button onClick={this.handleCheckout}>Checkout</button>
+		}
+
+		if (formnum === 1) {
+			page = <Login handleNext={this.handleNext}/>
+		}
+
+		if (formnum === 2) {
+			page = <Address handleNext={this.handleNext}/>
+		}
+
+		if (formnum === 3) {
+			page = <Payment handleNext={this.handleNext}/>
+		}
+
+		if (formnum === 4) {
+			page = <Confirmation returnHome={this.returnHome} login={this.state.login.name} address={this.state.address.line1}/>
+		}
+
 		return (
-			<div>
-				<h2>{this.state.login.name}</h2>
-				<button onClick={this.handleCheckout}>Checkout</button>
+			<div formum = {this.state.formnum}>
+				{page}
 			</div>
 		)
 	}
 	
 }
 
-
-var login = function(props) {
+var Login = function(props) {
+ return (
 	<div>Log In
 		<form>
 			<input type="text" name="name"/>
 			<input type="text" name="email"/>
 			<input type="text" name="password"/>
 		</form>
+		<button onClick={props.handleNext}>Next</button>
 	</div>
+	)
+}
+
+var Address = function(props) {
+	return (
+		<div>Address
+			<form>
+				<input type="text" name="line1"/>
+				<input type="text" name="line2"/>
+				<input type="text" name="city"/>
+				<input type="text" name="state"/>
+				<input type="text" name="zip"/>	
+				<input type="text" name="phone"/>			
+			</form>
+			<button onClick={props.handleNext}>Next</button>
+		</div>
+	)
+}
+
+var Payment = function(props) {
+	return (
+		<div>Payment
+			<form>
+				<input type="text" name="cc"/>
+				<input type="text" name="expdate"/>
+				<input type="text" name="cvv"/>
+				<input type="text" name="billzip"/>		
+			</form>
+			<button onClick={props.handleNext}>Next</button>
+		</div>
+	)
+}
+
+var Confirmation = function(props) {
+	return (
+		<div>Confirmation
+			<div>Name: {props.login}</div>
+			<div>Address: {props.address}</div>
+			<button onClick={props.returnHome}>Purchase</button>
+		</div>
+	)
 }
 
 
